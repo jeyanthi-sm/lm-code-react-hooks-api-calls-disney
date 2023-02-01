f14bbb94ed65d86223bc89fb36ff07b14de65b71
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-02-01 14:01:09 +0000

    Returning a default image if there is no imageurl from the API call and also stripping off the excess characters if there is one ordo nothing

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,9 +1,16 @@
-import { DisneyCharacter } from "../disney_character"
+import { DisneyCharacter } from "../disney_character";
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
+const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) =>  {
   
+  // Define a default in case the character doesn't have an image
+  let imageSource = "https://picsum.photos/300/200/?blur";
+  if (character.imageUrl) {
+    // API seems to include extra path for images so here we strip it off to fetch raw image
+    imageSource = character.imageUrl.substring(0, character.imageUrl.length || character.imageUrl.indexOf('/revision'));
+  }
+  return (
     <article className="character-item">
 
       <h2>{character.name}</h2>
@@ -11,11 +18,10 @@ const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) =>
       <div className="character-item__actions">
         Add to Favourites
       </div>
-
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+      <img className="character-item__img" src={imageSource} alt={character.name} />
 
     </article>
-  
+) }
 
 
 export default Character
